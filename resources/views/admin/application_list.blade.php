237ab6c1cb6f1c136af9a04578a14848301fe06e
AdlanAkmal21 <44663544+AdlanAkmal21@users.noreply.github.com>
2021-01-27 21:03:27 +0800

    27-1-2021 9:03PM

--- resources/views/admin/application_list.blade.php
+++ resources/views/admin/application_list.blade.php
@@ -16,51 +16,47 @@
                     <h1 class="h3 mb-0 text-gray-800">Applications List (All)</h1>
                 </div>
 
+
+                @if($applications->isEmpty())
                 <div class="card shadow-sm border-left-warning mt-4 mb-4">
                     <div class="card-body text-center">
-                        @if($applications->isEmpty())
-
                         <span class="text-muted">No Application Available</span>
-
-                        @else
-                        <div class="table-responsive-lg">
-                            <table class="table table-sm table-bordered table-striped container small">
-                                <thead class="table-dark">
-                                    <tr class="d-flex">
-                                        <th class="col-1">#</th>
-                                        <th class="col-3">Employee Name</th>
-                                        <th class="col-2">Leave Type</th>
-                                        <th class="col-3">Applied At</th>
-                                        <th class="col-2">Status</th>
-                                        <th class="col-1">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($applications as $key => $application)
-                                    <tr class="d-flex">
-                                        <td class="col-1">{{ $applications->firstItem() + $key }}.</td>
-                                        <td class="col-3">
-                                            <a
-                                                href="{{ route('admins.empshow', $application->user->id)}}">{{ $application->user->name }}</a>
-                                        </td>
-                                        <td class="col-2">{{ $application->refLeaveType->leave_type_name }}</td>
-                                        <td class="col-3">{{ $application->created_at }}</td>
-                                        <td class="col-2">{{ $application->refAppStatus->application_status_name }}</td>
-                                        <td class="col-1">
-                                            <a class="btn btn-primary"
-                                                href="{{ route('applications.adminAppShow', $application->leave_applications_id) }}">Show</a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                        @endif
                     </div>
-                    @if ($applications->isNotEmpty())
-                    <div class="card-footer">{{ $applications->links() }}</div>
-                    @endif
                 </div>
+                @else
+                <div class="table-responsive-lg mt-3">
+                    <table class="table table-sm table-bordered table-striped container small">
+                        <thead class="table-dark">
+                            <tr class="d-flex">
+                                <th class="col-1">#</th>
+                                <th class="col-2">Leave Type</th>
+                                <th class="col-2">From</th>
+                                <th class="col-2">To</th>
+                                <th class="col-2">Applied At</th>
+                                <th class="col-2">Status</th>
+                                <th class="col-1">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($applications as $key => $application)
+                            <tr class="d-flex">
+                                <td class="col-1">{{ $applications->firstItem() + $key }}.</td>
+                                <td class="col-2">{{ $application->refLeaveType->leave_type_name }}</td>
+                                <td class="col-2">{{ $application->from }}</td>
+                                <td class="col-2">{{ $application->to }}</td>
+                                <td class="col-2">{{ $application->created_at }}</td>
+                                <td class="col-2">{{ $application->refAppStatus->application_status_name }}</td>
+                                <td class="col-1">
+                                    <a class="btn btn-primary"
+                                        href="{{ route('applications.adminAppShow', $application->leave_applications_id) }}">Show</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                {{ $applications->links() }}
+                @endif
 
 
 
